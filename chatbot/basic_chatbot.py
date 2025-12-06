dc309722be97ea2ce2cf550f333e28733082eeda
Tanish Rajput <tanishrajput9@gmail.com>
2025-12-06 21:27:09 +0530

    added a loop for chatbot

--- chatbot/basic_chatbot.py
+++ chatbot/basic_chatbot.py
@@ -1,5 +1,6 @@
 from langgraph.graph import START, END, StateGraph
 from langgraph.graph.message import add_messages # kind of reducer
+from langgraph.checkpoint.memory import MemorySaver
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import BaseMessage, HumanMessage
 from typing import TypedDict, Annotated
@@ -23,6 +24,8 @@ def chat_node(state : ChatState):
 
     return {"messages" : [response]}
 
+checkpointer = MemorySaver()
+
 # Create graph
 graph = StateGraph(ChatState)
 
@@ -34,12 +37,41 @@ graph.add_edge(START,'chat_node')
 graph.add_edge('chat_node',END)
 
 # compile graph
-chatbot = graph.compile()
+chatbot = graph.compile(checkpointer=checkpointer)
 
-png_bytes = chatbot.get_graph().draw_mermaid_png()
+# For downloading the Graph Image
+# png_bytes = chatbot.get_graph().draw_mermaid_png()
 
-output_file = "chatbot/basic_chatbot.png"
+# output_file = "chatbot/basic_chatbot.png"
 
-# Write the PNG to disk
-with open(output_file, "wb") as f:
-    f.write(png_bytes)
+# # Write the PNG to disk
+# with open(output_file, "wb") as f:
+#     f.write(png_bytes)
+
+# initial_state = {
+#     'messages' : [HumanMessage(content="What is the capital of India?")]
+# }
+
+# response = chatbot.invoke(initial_state)['messages'][-1].content
+
+# print(response)
+
+thread_id = '1'
+
+while True:
+    user_message = input("Type here : ")
+
+    print("User : ", user_message)
+
+    if user_message.strip().lower() in ['exit', 'quit', 'bye']:
+        break
+
+    config = {'configurable' : {'thread_id' : thread_id}}
+
+    response = chatbot.invoke({'messages' : [HumanMessage(content=user_message)]},config=config)
+
+    print('AI : ', response['messages'][-1].content)
+
+
+
+
