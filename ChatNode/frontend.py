7c1c20ab9cd89c134131d18ab5c31ed2b58bc38d
Tanish Rajput <tanishrajput9@gmail.com>
2025-12-11 21:12:48 +0530

    Implemented stremalit ui for ChatNode

---
+++ ChatNode/frontend.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from backend import chatbot
+from langchain_core.messages import HumanMessage
+
+CONFIG = {'configurable' : {'thread_id' : 'thread-1'}}
+# st.session_state -> dict ->
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+# {'role' : 'user','content':'hi'}
+# {'role' : 'assistant','content':'hello'}
+
+# Loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input("Type here")
+
+if user_input:
+    # Store user message in message_history
+    st.session_state['message_history'].append({'role' : 'user','content' : user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+    
+    response = chatbot.invoke({'messages' : [HumanMessage(content=user_input)]},config=CONFIG)
+    ai_message = response['messages'][-1].content
+
+    # Store AI message in message_history
+    st.session_state['message_history'].append({'role' : 'assistant','content' : ai_message})
+    with st.chat_message('assistant'):
+        st.text(ai_message)
