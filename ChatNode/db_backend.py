a5b8ce640149e7c076ba5ded2295c66dea705786
Tanish Rajput <tanishrajput9@gmail.com>
2025-12-17 11:00:27 +0530

    Added a function to retrieve all threads

--- ChatNode/db_backend.py
+++ ChatNode/db_backend.py
@@ -46,3 +46,10 @@ graph.add_edge('chat_node',END)
 
 # Compile graph
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+    
+    return list(all_threads)
