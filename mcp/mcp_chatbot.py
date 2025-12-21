31917aaaf200697c316b96edc4c9877604b555fc
Tanish Rajput <tanishrajput9@gmail.com>
2025-12-21 20:41:45 +0530

    Integrated tools from a local mcp server by creating mcp client

---
+++ mcp/mcp_chatbot.py
@@ -0,0 +1,68 @@
+from langgraph.graph import StateGraph, START
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_community.tools import DuckDuckGoSearchRun
+from typing import TypedDict,Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.tools import tool
+import asyncio
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+load_dotenv()  
+
+llm = ChatOpenAI(model="gpt-5")
+
+# MCP client for local mcp server
+client = MultiServerMCPClient(
+    {
+        'arith' : {
+            'transport' : 'stdio',
+            'command' : 'python3',
+            'args' : '/Users/tanishrajput/Desktop/Data Science/Generative AI/MCP/math/main.py'
+        }
+    }
+)
+
+# state
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+async def build_graph():
+    # tools
+    tools = await client.get_tools()
+
+    llm_with_tools = llm.bind_tools(tools)
+    # nodes
+    async def chat_node(state: ChatState):
+        messages = state["messages"]
+        response = await llm_with_tools.ainvoke(messages)
+        return {'messages': [response]}
+
+    tool_node = ToolNode(tools)
+
+    # defining graph and nodes
+    graph = StateGraph(ChatState)
+
+    graph.add_node("chat_node", chat_node)
+    graph.add_node("tools", tool_node)
+
+    # defining graph connections
+    graph.add_edge(START, "chat_node")
+    graph.add_conditional_edges("chat_node", tools_condition)
+    graph.add_edge("tools", "chat_node")
+
+    chatbot = graph.compile()
+    
+    return chatbot
+
+async def main():
+    chatbot = await build_graph()
+
+    result = await chatbot.ainvoke({"messages": [HumanMessage(content="Find the modulus of 132354 and 23 and give answer like a cricket commentator.")]})
+
+    print(result['messages'][-1].content)
+
+if __name__ == '__main__':
+    asyncio.run(main())
